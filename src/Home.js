da328c2145c69c1a3b1efbe5d02fc713ff5c2724
Harshal-9 <harshal.kodgire@walchandsangli.ac.in>
2021-10-27 15:07:24 +0530

    useEffect Hook in react

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -19,6 +19,10 @@ const Home = () => {
         const newBlogs = blogs.filter( (blog) => blog.id !== id )
         setBlogs(newBlogs);
     }
+
+    useEffect(()=>{
+        console.log("Use Effect Triggered !");
+    });
 
     return ( 
         <div className="home">
